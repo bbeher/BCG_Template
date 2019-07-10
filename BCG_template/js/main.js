12d88e0d075d44a53aa6ec99d98b482aa008c701
Biswajit <biswa4win@gmail.com>
2019-07-10 19:00:55 +0530

    BCG Template files

---
+++ BCG_template/js/main.js
@@ -0,0 +1,52 @@
+(function($) {
+  
+  "use strict";
+  
+  /* Page Loader active
+  ========================================================*/
+  $('#preloader').fadeOut();
+
+  /* 
+   One Page Navigation
+   ========================================================================== */
+
+
+    $(window).on('load', function() {
+       
+        $('body').scrollspy({
+            target: '.navbar-collapse',
+            offset: 195
+        });
+
+        $(window).on('scroll', function() {
+            if ($(window).scrollTop() > 100) {
+                $('.fixed-top').addClass('menu-bg');
+            } else {
+                $('.fixed-top').removeClass('menu-bg');
+            }
+        });
+
+    });
+
+    $('.navbar-nav .nav-link').click(function(){
+      $(this).addClass("active").parent().siblings().children().removeClass("active");
+
+    });
+
+  /* Auto Close Responsive Navbar on Click
+  ========================================================*/
+  function close_toggle() {
+      if ($(window).width() <= 768) {
+          $('.navbar-collapse a').on('click', function () {
+              $('.navbar-collapse').collapse('hide');
+          });
+      }
+      else {
+          $('.navbar .navbar-inverse a').off('click');
+      }
+  }
+  close_toggle();
+  $(window).resize(close_toggle);
+
+}(jQuery));
+
